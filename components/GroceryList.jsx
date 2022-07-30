4d2992cce0c3179feb2c3da0d807e901b752d736
Solomon Schuster <solomon.schuster01@gmail.com>
2022-07-30 10:46:51 -0700

    added list and delete function

--- components/GroceryList.jsx
+++ components/GroceryList.jsx
@@ -1,16 +1,40 @@
 function GroceryList(){
-    const [newItem, setNewItem] = React.useState('');
+    const [item, setItem] = React.useState('');
     const [count, setCount] = React.useState(0);
-
-    function handleSubmit() {
-        console.log(`${newItem}: ${count}`);
+    const [groceryList, setGroceryList] = React.useState([]);
+    
+    
+    function addToList() {
+        console.log(`${item}: ${count}`);
+        setGroceryList([...groceryList, {name: item, amount: count}]);
+        console.log(groceryList);
     }
+    
+    function deleteItem(index) {
+        let newList = groceryList.filter((item, i) => index != i);
+
+        setGroceryList(newList);
+    }
+    
+    let list = groceryList.map((item, index) => {
+        return (
+            <li key={index} onClick={() => deleteItem(index)}>
+                {item.name}: {item.amount}
+            </li>
+        )
+    });
 
     return (
         <React.Fragment>
-            <input type='text' id='item' placeholder="Enter grocery item" onChange={(e) => setNewItem(e.target.value)}/><br/>
+            <input type='text' id='item' placeholder="Enter grocery item" onChange={(e) => setItem(e.target.value)}/><br/>
             <input type='number' id='count' placeholder="Amount of item" onChange={e => setCount(e.target.value)}/><br/>
-            <button onClick={handleSubmit}>Submit</button>
+            <button onClick={addToList}>Add to Cart</button>
+
+            <h1>List: </h1>
+            <ol>
+                {list}
+            </ol>
+            
             
         </React.Fragment>
     )
