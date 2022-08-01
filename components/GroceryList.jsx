6fb7c3c594f88c0e7cfc16018b7e03c5f00f69cb
Ted Graf <tedgrafx@gmail.com>
2022-08-01 07:35:56 -0700

    tried adding price

--- components/GroceryList.jsx
+++ components/GroceryList.jsx
@@ -1,12 +1,13 @@
 function GroceryList(){
     const [item, setItem] = React.useState('');
     const [count, setCount] = React.useState(0);
+    const [price, setPrice] = React.useState('');
     const [groceryList, setGroceryList] = React.useState([]);
     
     
     function addToList() {
         console.log(`adding ${item}: ${count} to list`);
-        setGroceryList([...groceryList, {name: item, amount: count}]);
+        setGroceryList([...groceryList, {name: item, amount: count, cost: price}]);
         console.log(groceryList);
     }
     
@@ -18,22 +19,24 @@ function GroceryList(){
     
     let list = groceryList.map((item, index) => {
         return (
-            <li key={index} onClick={() => deleteItem(index)}>
-                {item.name}: {item.amount}
+            <li key={index}>
+                {item.name}: {item.amount} - {item.price}
+                <button onClick={() => deleteItem(index)}>Remove</button>
             </li>
         )
     });
 
     return (
         <React.Fragment>
-            <input type='text' id='item' placeholder="Enter grocery item" onChange={(e) => setItem(e.target.value)}/><br/>
-            <input type='number' id='count' placeholder="Amount of item" onChange={e => setCount(e.target.value)}/><br/>
+            <input type='text' id='item' placeholder="Grocery item" onChange={(e) => setItem(e.target.value)}/><br/>
+            <input type='number' id='count' placeholder="Quantity" onChange={e => setCount(e.target.value)}/><br/>
+            <input type='number' id='price' placeholder="Cost per item" onChange={e => setPrice(e.target.value)}/><br/>
             <button onClick={addToList}>Add to Cart</button>
 
             <h1>List: </h1>
-            <ol>
+            <ul>
                 {list}
-            </ol>
+            </ul>
             
             
         </React.Fragment>
